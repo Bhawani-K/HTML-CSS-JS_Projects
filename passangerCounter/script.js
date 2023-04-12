d5c6a238cea3f1e993667e0552bf328def11393b
Bhawani_K <kundala.bhawani@outlook.com>
2023-04-12 20:18:05 +0530

    feature_AddedIncrement&SaveFeature

--- passangerCounter/script.js
+++ passangerCounter/script.js
@@ -1,1 +1,15 @@
-document.getElementById('count').innerText = 5;
+/* `document.getElementById('count');` is selecting the HTML element with the `id`attribute of "count" and storing it in the `countEl` variable. */
+let countEl = document.getElementById('count');
+let saveEl = document.getElementById('saveEntries')
+let count = 0;
+
+function increment() {
+    count = count + 1;
+    countEl.textContent = count;
+}
+function save() {
+    let countStr = count + '-';
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
+}
